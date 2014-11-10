74aed4ad5b43df5c88628403d53c2f31c8e5ca4a
渡世白玉 <dushibaiyu@yahoo.com>
2014-11-10 09:28:10 +0800

    更新readme

--- common/datastruct.h
+++ common/datastruct.h
@@ -3,21 +3,7 @@
 #include <QDataStream>
 #include <QByteArray>
 #include <QDebug>
-/***
- * TODO: next:
- *  添加加密，安全和快速的加密算法
- *  优化下服务端的进程架构，尽量直接线程建立socket，取消用moveToThread
- *  服务端替换底层事件循环系统，用libev的事件替换，这样在linux，bsd，mac下就能有更好的性能。
- *  测试压缩，看下性能，考虑是否压缩传输数据。
- *
- * TODO: Future
- *  登录认证模块独立出来，动态返回服务器地址，多服务器的话，可以达到负载均衡
- *  服务端加上数据库，管理用户和token，也可以登录用http，和http交互。
- *  如果用户系统完善，添加流量统计。
- *  客户端界面设计优化
- *  考虑加入其他代理协议。
- *  如果有效率更高的，更换数据序列化，和传输的数据结构。
- * */
+
 
 #define DATASTREAM_VISION QDataStream::Qt_5_2
 /****
